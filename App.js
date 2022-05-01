32f69a69e552914522372051900ee64f2e9195e7
abhishek giri <giriji7571@gmail.com>
2022-05-01 22:59:45 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,118 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image , Pressable} from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Camera } from 'expo-camera';
+import { shareAsync } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
+
+export default function App() {
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const [photo, setPhoto] = useState();
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+  }, []);
+
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>
+  }
+
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false
+    };
+
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+        alert("picture saved in gallery")
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        <View style={{flexDirection:"row",margin:5}}>
+         
+        <Button title="Share" onPress={sharePic} />
+        <View style={{marginRight:15, marginLeft:15}}>
+        {hasMediaLibraryPermission ? <Button title="Save" color={"red"} onPress={savePhoto} /> : undefined}
+            </View>
+       
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        {/* <Button title="Take Pic" onPress={takePic} /> */}
+        <Pressable style={styles.button} onPress={takePic}>
+      <Text style={styles.text}>Take pic</Text>
+    </Pressable>
+    
+      </View>
+      <StatusBar style="auto" />
+    </Camera>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  buttonContainer: {
+    //backgroundColor: '#fff',
+    //alignSelf: "baseline"
+    
+  },
+  preview: {
+    alignSelf: 'stretch',
+    flex: 1
+  },
+  button:{
+    //alignSelf: "center",
+    //position: 'absolute',
+    marginTop:650,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
